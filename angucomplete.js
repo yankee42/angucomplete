67a5e333369492617f9723130ab12b1a1be5f970
Yannik Hampe <yzeugs@cipher-code.de>
2015-02-06 23:09:06 +0100

    Remove unused parameters

--- angucomplete.js
+++ angucomplete.js
@@ -32,7 +32,7 @@ angular.module('angucomplete', [] )
         },
         template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-click="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
 
-        link: function($scope, elem, attrs) {
+        link: function($scope, elem) {
             $scope.lastSearchTerm = null;
             $scope.currentIndex = null;
             $scope.justChanged = false;
@@ -136,11 +136,11 @@ angular.module('angucomplete', [] )
 
                     } else {
                         $http.get($scope.url + str, {}).
-                            success(function(responseData, status, headers, config) {
+                            success(function(responseData) {
                                 $scope.searching = false;
                                 $scope.processResults((($scope.dataField) ? responseData[$scope.dataField] : responseData ), str);
                             }).
-                            error(function(data, status, headers, config) {
+                            error(function() {
                                 console.log("error");
                             });
                     }
